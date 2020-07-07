b1af63235893274e820707a0f0d3360f7855fb38
IAmMahendraSharma <mkbls77@gmail.com>
2020-07-07 11:04:24 +0530

    Timer updates

--- js/config.js
+++ js/config.js
@@ -2,5 +2,5 @@
 var subscribeUrl = "http://octrace.us14.list-manage.com/subscribe/post?u=b65c95763e0ae8e0c14e7af0e&amp;id=7e9cf89638";
 var email = "octrace@gmail.com";
 var countdown = true;
-var configDate = "2020 07 07 17:00";
+var configDate = "2020 07 07 16:00";
 var particles = true;
